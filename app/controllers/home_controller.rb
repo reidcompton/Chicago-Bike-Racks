ee49f816c40f1ade66cba2df66150a8deda12523
reidcompton <reidcompton@mac.com>
2016-03-28 16:29:25 -0500

    https

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,7 @@ class HomeController < ApplicationController
 	def getBikesByLatLong 
 		lat = params[:lat]
 		long = params[:long]
-		response = HTTParty.get("http://data.cityofchicago.org/resource/cbyb-69xx.json?$select=location.longitude%2C%20location.latitude&$where=within_circle(location%2C%20" + lat + "%2C%20" + long + "%2C%20322)")
+		response = HTTParty.get("https://data.cityofchicago.org/resource/cbyb-69xx.json?$select=location.longitude%2C%20location.latitude&$where=within_circle(location%2C%20" + lat + "%2C%20" + long + "%2C%20322)")
 		render :json => response
 	end
 
